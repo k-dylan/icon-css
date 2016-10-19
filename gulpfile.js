f27c40fe342faa10b4cd37be465bfb147ab25153
kdylan <kdylan@qq.com>
2016-10-19 10:28:08 +0800

    修改目录结构

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var stylus = require('gulp-stylus');
 gulp.task('server', function () {
     connect.server({
         port: 3000,
-        root: './',
+        root: './dist',
         livereload: true
     });
 });
